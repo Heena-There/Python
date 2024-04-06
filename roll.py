2d8ffa9cb7ef733e280900bcba7c498590b97311
DBDA d. <DBDA@dypm.org>
2024-04-06 16:56:50 +0530

    changes done

---
+++ roll.py
@@ -0,0 +1,9 @@
+# name = input("Enter your name : ")
+# rollno = input("Enter your roll no :")
+# print(name,rollno, sep=" ",end=" ")
+# print("Name:",name,"Roll no:",rollno,sep=" ",end=" ")
+
+var1=10
+print("memory location=",id(var1))
+var2=var1
+print("memory location=",id(var2))
